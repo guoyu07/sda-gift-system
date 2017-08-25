065bb4a9dfaca8956fbdfa162466438ca38a6b03
jianlai2007 <jianlai2007@qq.com>
2017-08-25 16:59:21 +0800

    搭建框架

--- src/main/java/com/sda/gift/controller/LoginController.java
+++ src/main/java/com/sda/gift/controller/LoginController.java
@@ -1,7 +1,15 @@
 package com.sda.gift.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Allen on 2017/8/24.
  */
+@Controller
 public class LoginController {
+    @RequestMapping("/")
+    public String login(){
+        return "Login";
+    }
 }

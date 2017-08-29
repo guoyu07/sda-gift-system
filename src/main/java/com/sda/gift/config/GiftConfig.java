ac0b87d59b925c9cda309deafb5a1b919e3f2553
jianlai2007 <jianlai2007@qq.com>
2017-08-29 12:03:07 +0800

    搭建框架

--- src/main/java/com/sda/gift/config/GiftConfig.java
+++ src/main/java/com/sda/gift/config/GiftConfig.java
@@ -2,12 +2,14 @@ package com.sda.gift.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Allen on 2017/8/29.
  */
 @ConfigurationProperties(prefix = "gift")
 @Data
+@Component
 public class GiftConfig {
     private String activityName;
 }

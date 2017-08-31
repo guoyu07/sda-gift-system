c98ce15de2ac6ec1df2dcaf9454ed9e0ba7846a2
jianlai2007 <jianlai2007@qq.com>
2017-08-31 13:17:12 +0800

    搭建框架

--- src/main/java/com/sda/gift/controller/ProductController.java
+++ src/main/java/com/sda/gift/controller/ProductController.java
@@ -15,6 +15,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -48,6 +49,7 @@ public class ProductController {
         return mv;
     }
     @PostMapping("/chooseProduct")
+    @ResponseBody
     public void chooseProduct(HttpServletRequest request){
         List<ProductEntity> pros = productService.list();
         List<OrderEntity> odrList = new ArrayList<>();

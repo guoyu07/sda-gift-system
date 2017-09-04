989d21eea28d0d63761238d1b30c7c1700e3cf99
jianlai2007 <jianlai2007@qq.com>
2017-09-04 16:59:37 +0800

    搭建框架

--- src/main/java/com/sda/gift/SdaGiftSystemApplication.java
+++ src/main/java/com/sda/gift/SdaGiftSystemApplication.java
@@ -2,11 +2,14 @@ package com.sda.gift;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
+@EnableAutoConfiguration
+@ComponentScan
 @MapperScan("com.sda.gift.mapper")
 public class SdaGiftSystemApplication {
 
